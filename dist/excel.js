9aca3a27991f14172fd74e0a81e9f559a7d903ce
comame <dev@comame.xyz>
2021-03-23 15:35:39 +0900

    target es5

--- dist/excel.js
+++ dist/excel.js
@@ -4,29 +4,32 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.parseExcelToJson = void 0;
-const xlsx_1 = __importDefault(require("xlsx"));
+var xlsx_1 = __importDefault(require("xlsx"));
 function parseExcelToJson(excel) {
-    const data = {};
-    const file = xlsx_1.default.read(excel);
-    for (const sheetName of file.SheetNames) {
-        const tokens = parseSheet(file.Sheets[sheetName]);
+    var data = {};
+    var file = xlsx_1.default.read(excel);
+    for (var _i = 0, _a = file.SheetNames; _i < _a.length; _i++) {
+        var sheetName = _a[_i];
+        var tokens = parseSheet(file.Sheets[sheetName]);
         parse(tokens, data);
     }
     return data;
 }
 exports.parseExcelToJson = parseExcelToJson;
 function parseSheet(sheet) {
-    const keyColumnCellNames = Object.keys(sheet).filter(it => it.startsWith('A')).sort((a, b) => {
-        const aNum = Number.parseInt(a.slice(1));
-        const bNum = Number.parseInt(b.slice(1));
+    var _a, _b;
+    var keyColumnCellNames = Object.keys(sheet).filter(function (it) { return it.startsWith('A'); }).sort(function (a, b) {
+        var aNum = Number.parseInt(a.slice(1));
+        var bNum = Number.parseInt(b.slice(1));
         return aNum - bNum;
     });
-    let started = false;
-    const data = [];
-    for (const keyCellName of keyColumnCellNames) {
-        const key = sheet[keyCellName].w;
+    var started = false;
+    var data = [];
+    for (var _i = 0, keyColumnCellNames_1 = keyColumnCellNames; _i < keyColumnCellNames_1.length; _i++) {
+        var keyCellName = keyColumnCellNames_1[_i];
+        var key = sheet[keyCellName].w;
         if (typeof key !== 'string') {
-            throw Error(`${keyCellName} (${key}) is not a string value.`);
+            throw Error(keyCellName + " (" + key + ") is not a string value.");
         }
         if (key === '__' && !started) {
             started = true;
@@ -37,47 +40,51 @@ function parseSheet(sheet) {
         }
         if (key.startsWith('  '))
             continue;
-        const valueCellName = 'B' + keyCellName.slice(1);
-        const value = sheet[valueCellName]?.w ?? '';
+        var valueCellName = 'B' + keyCellName.slice(1);
+        var value = (_b = (_a = sheet[valueCellName]) === null || _a === void 0 ? void 0 : _a.w) !== null && _b !== void 0 ? _b : '';
         if (key.startsWith(' ')) {
             data.push({
                 type: 'array',
                 key: key.slice(1),
-                value
+                value: value
             });
         }
         else if (key.startsWith('\t\t')) {
             data.push({
                 type: 'h2',
                 key: key.slice(2),
-                value
+                value: value
             });
         }
         else if (key.startsWith('\t')) {
             data.push({
                 type: 'h1',
                 key: key.slice(1),
-                value
+                value: value
             });
         }
         else {
             data.push({
                 type: 'normal',
-                key,
-                value
+                key: key,
+                value: value
             });
         }
     }
-    throw Error(`末尾に __ がありません`);
+    throw Error("\u672B\u5C3E\u306B __ \u304C\u3042\u308A\u307E\u305B\u3093");
 }
-function parse(tokens, data = {}) {
-    if (tokens[0]?.type !== 'h1') {
+function parse(tokens, data) {
+    var _a, _b;
+    var _c;
+    if (data === void 0) { data = {}; }
+    if (((_c = tokens[0]) === null || _c === void 0 ? void 0 : _c.type) !== 'h1') {
         throw Error('最初の行がシート名ではありません');
     }
-    const currentSheetObj = {};
-    let currentH2 = null;
-    let firstArrayKey = '';
-    for (const token of tokens.slice(1)) {
+    var currentSheetObj = {};
+    var currentH2 = null;
+    var firstArrayKey = '';
+    for (var _i = 0, _d = tokens.slice(1); _i < _d.length; _i++) {
+        var token = _d[_i];
         if (token.type === 'normal') {
             if (currentH2 === null) {
                 currentSheetObj[token.key] = token.value;
@@ -103,20 +110,20 @@ function parse(tokens, data = {}) {
                 throw Error('小項目に配列を含むとき、その小項目では他の要素は使用できません。');
             }
             if (!Array.isArray(currentSheetObj[currentH2])) {
-                currentSheetObj[currentH2] = [{
-                        [token.key]: token.value
-                    }];
+                currentSheetObj[currentH2] = [(_a = {},
+                        _a[token.key] = token.value,
+                        _a)];
                 firstArrayKey = token.key;
             }
             else {
-                const self = currentSheetObj[currentH2];
+                var self_1 = currentSheetObj[currentH2];
                 if (token.key === firstArrayKey) {
-                    self.push({
-                        [token.key]: token.value
-                    });
+                    self_1.push((_b = {},
+                        _b[token.key] = token.value,
+                        _b));
                 }
                 else {
-                    self[self.length - 1][token.key] = token.value;
+                    self_1[self_1.length - 1][token.key] = token.value;
                 }
             }
         }
--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.parse = void 0;
-const excel_1 = require("./excel");
+var excel_1 = require("./excel");
 exports.parse = excel_1.parseExcelToJson;
 //# sourceMappingURL=index.js.map
